f80313275bdb329119fa183313e8e7e03b9f830d
root <root@LAPTOP-8LA5CDLH.localdomain>
2020-10-10 23:36:02 +0800

    美化UI，完善细节部分

--- Downloader/Movies/m1905.php
+++ Downloader/Movies/m1905.php
@@ -12,14 +12,8 @@ $urls = [
     // 添加更多M3U8地址信息 .....
     "https://m3u8i.vodfile.m1905.com/202010110619/88d6e6252f5887de7f14b7f928b1fe6d/movie/2015/01/07/m20150107OSHOA61YXCI9UYYJ/m20150107OSHOA61YXCI9UYYJ-561k.m3u8",
     "https://m3u8i.vodfile.m1905.com/202010110618/49b5604740f90d852e759d871d27d0be/movie/1206/1206285AA18C08D8615A04-564k.m3u8",
-    "https://m3u8i.vodfile.m1905.com/202010110620/c006649b8bbf91d7df55c9ff904d44af/movie/2014/07/10/m20140710LP24JW3CN8IB86H6/m20140710LP24JW3CN8IB86H6-534k.m3u8"
+//    "https://m3u8i.vodfile.m1905.com/202010110620/c006649b8bbf91d7df55c9ff904d44af/movie/2014/07/10/m20140710LP24JW3CN8IB86H6/m20140710LP24JW3CN8IB86H6-534k.m3u8"
 ];
-
-
-/**************************************************************
- * Parsers 目录
- **************************************************************/
-
 
 // 保存到本地电脑指定目录
 $local = '/mnt/c/Users/twomiao/desktop/download';
