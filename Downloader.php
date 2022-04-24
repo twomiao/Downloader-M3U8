10f10110ce1cbcb9ea101385a90da85ffe019a2e
twomiao <995200452>
2022-04-25 01:08:37 +0800

    换行

--- Downloader.php
+++ Downloader.php
@@ -1,6 +1,7 @@
 <?php declare(strict_types=1);
 require __DIR__ . '/vendor/autoload.php';
 
+use Swoole\Process;
 use Swoole\Runtime;
 use Downloader\Runner\Downloader;
 use Symfony\Component\Console\Application;
@@ -15,6 +16,9 @@ run(function () {
     $application = new Application(Downloader::PROGRAM_NAME, Downloader::VERSION);
     $application->setAutoExit(false);
     $application->add(new M1905Command()); // 下载 https://www.1905.com/ 视频
+    $application->add(new \Downloader\Command\M1906Command()); // 下载 https://www.1905.com/ 视频
+    $application->add(new \Downloader\Command\CopyCommand()); // 下载 https://www.1905.com/ 视频
+    $application->add(new \Downloader\Command\M1907Command()); // 下载 https://www.1905.com/ 视频
 //    $application->add(new OtherCommand()); // 其它网站视频
 
     $application->run();
