6f9a3bd1f0e5cc89d2e6c03667914ba528108f64
twomiao <995200452@qq.com>
2022-12-13 16:28:34 +0800

    丢弃输出

--- Runner/CreateFFmpegVideoListener.php
+++ Runner/CreateFFmpegVideoListener.php
@@ -13,9 +13,9 @@ class CreateFFmpegVideoListener
         // 本地临时文件名称
         $realFile = $file->getRealFilename();
         $tempDir  = $file->getTempDir();
+        $tempFile = $file->getTempFilePath();
 
         $ffmpegTextFile = \dirname($file->getTempFilePath()). '/'.$file->getTempFilename().'.txt';
-        $tempFile       = $file->getTempFilePath();
 
         print PHP_EOL;
 //        print date('Y-m-d H:i:s'). " >正在生成[FFMPEG 命令]文件: {$ffmpegTextFile}.".PHP_EOL;
@@ -23,18 +23,21 @@ class CreateFFmpegVideoListener
         if ( !$success = self::ffmpegFilePath($file, $ffmpegTextFile ) ) {
             throw new \Exception("FFMPEG命令生成失败: {$file->getTempFilePath()}.");
         }
+        print "正在创建本地视频文件 [{$realFile}].".PHP_EOL;
 //        $command = "/usr/bin/ffmpeg -f concat -safe 0 -i {$ffmpegTextFile}  -c copy  {$tempFile} >/dev/null 2>&1";
-        $command = "/usr/bin/ffmpeg -f concat -safe 0 -i {$ffmpegTextFile}  -c copy  {$tempFile}";
+        $command = "/usr/bin/ffmpeg -f concat -safe 0 -i {$ffmpegTextFile}  -c copy  {$tempFile} >/dev/null 2>&1";
 //        $command = "/usr/bin/ffmpeg -f concat -safe 0 -i {$ffmpegTextFile}  -c copy  {$filename}";
         print date('Y-m-d H:i:s'). " >正在执行FFMPEG命令, 创建视频文件: {$realFile}.\n";
         try {
             if( !$res = System::exec($command)) {
                 throw new \Exception("FFMPEG命令执行失败：{$command}");
             }
+            // 拷贝完成, 执行删除
+            \copy($tempFile, $realFile);
         } finally {
            \is_file($ffmpegTextFile) && \unlink($ffmpegTextFile);
+            @\unlink($tempFile);
         }
-        \rename($tempFile, $realFile);
         print date('Y-m-d H:i:s'). " >正在删除[{$realFile}]的分片文件 .....".PHP_EOL;
         $this->deleteFiles($file);
         is_dir($tempDir) && \rmdir($tempDir);
